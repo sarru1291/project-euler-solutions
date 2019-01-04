351effd3242cc95a19e15f57e9b18da27cf4195e
sarru1291 <sarru1291@gmail.com>
2019-01-04 16:04:31 +0530

    problem 2 initialised

--- Solution1/Solution1.java
+++ Solution1/Solution1.java
@@ -7,17 +7,11 @@ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
 Find the sum of all the multiples of 3 or 5 below 1000.
 */
 public class Solution1{
-  static boolean isMultiple(int x){
-      if(x%3==0||x%5==0){
-        return true;
-      }
-    return false;
-  }
   public static void main(String... args){
     final int limit=1000;
     int sum=0;
     for(int i=1;i<limit;i++){
-          if(isMultiple(i)){
+          if(i%3==0||i%5==0){
               sum=sum+i;
           }
     }
